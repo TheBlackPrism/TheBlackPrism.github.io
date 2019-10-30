daa14727380ddac3af7b49a9462341a0dfcd2c50
Yves Lütjens <yves@luetjens.ch>
2019-10-30 16:19:20 +0100

    removed small mistake

--- js/prank/prank.js
+++ js/prank/prank.js
@@ -695,9 +695,6 @@ function superLogout () {
 
     const div = document.createElement('div')
     div.innerText = `Logging you out from ${name}...`
-
-    const logoutMessages = document.querySelector('.logout-messages')
-    logoutMessages.appendChild(div)
   }
 }
 
